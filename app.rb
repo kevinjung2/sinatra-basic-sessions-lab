185f81674abbc102a234f2506ab097542b39d071
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-26 20:14:17 -0500

    commit tree 1cf646f732af2096ad87a8dc6edd21fe6e348764

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, 'cd21b83fb6baff99c921210f9456a46d7fdfca49e277c891d6f0c3229945934a143c0f86b414806236b892d3ac543ab03a6715ebd347f0dc4aa68a1bbe4219e1'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @item = session[:item]
+    erb :show
+  end
 end
